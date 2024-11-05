2468f9f8c437755f03c96b8a0ba70c612d2ce8ae
exf1kzz <141450846+exf1kzz@users.noreply.github.com>
2024-11-05 19:27:07 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,5 +1,5 @@
 import math
-
+import unittest
 
 def area(r):
     '''	
@@ -25,3 +25,21 @@ def perimeter(r):
 			float: периметр круга вычисляемый по формуле: 2 * math.pi * r
     ''' 
     return 2 * math.pi * r
+
+
+class CircleTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+       
+    def test_area(self):
+        res = area(5)
+        self.assertAlmostEqual(res, 78.53981633974483, places=7)
+
+    def test_perimeter(self):
+        res = perimeter(5)
+        self.assertAlmostEqual(res, 31.41592653589793, places=7)
+        
+    def test_zero_perimeter(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
