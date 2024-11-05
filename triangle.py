a0cfefd44c163fe9659704a7c9db70bc7f438688
exf1kzz <141450846+exf1kzz@users.noreply.github.com>
2024-11-05 19:28:51 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,8 +1,53 @@
+import unittest
+
 def area(a, h): 
-    '''принимает числа а и h, возвращает площадь треугольника'''
+    '''
+    Возвращает площадь треугольника со стороной a и высотой h.
+
+        Args:
+            a (int): десятичное целое число
+            h (int): десятичное целое число
+
+        Returns:
+            float: площадь треугольника, вычисляемая по формуле a * h / 2
+
+        Example:
+            print(area(5, 5)) # Вывод: 12.5
+    '''
     return a * h / 2 
 
 def perimeter(a, b, c): 
-    '''принимает числа а, b и с, возвращаеат периметр трекгольника'''
+    '''
+    Возвращает периметр треугольника со сторонами a, b и c.
+
+        Args:
+            a (int): десятичное целое число
+            b (int): десятичное целое число
+            c (int): десятичное целое число
+
+        Returns:
+            int: периметр треугольника, вычисляемый по формуле a + b + c
+
+        Example:
+            print(perimeter(5, 5, 5)) # Вывод: 15
+    '''
     return a + b + c
+
+
+class TriangleTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        res = area(0, 5)
+        self.assertEqual(res, 0)
+       
+    def test_area(self):
+        res = area(15, 4)
+        self.assertEqual(res, 30)
+
+    def test_perimeter(self):
+        res = perimeter(5, 10, 6)
+        self.assertEqual(res, 21)
+        
+    def test_zero_perimeter(self):
+        res = perimeter(0, 0, 1)
+        self.assertEqual(res, 1)
 
