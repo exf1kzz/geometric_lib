55ae6ed92ab2fc3e03b1ba3be16297cb257a028e
exf1kzz <141450846+exf1kzz@users.noreply.github.com>
2024-12-03 17:18:29 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -51,3 +51,27 @@ class TriangleTestCase(unittest.TestCase):
         res = perimeter(0, 0, 1)
         self.assertEqual(res, 1)
 
+    def test_float_area(self):
+        res = area(5.5, 4.5)
+        self.assertEqual(res, 12.375)
+
+    def test_float_perimeter(self):
+        res = perimeter(5.5, 4.5, 3.5)
+        self.assertEqual(res, 13.5)
+
+    def test_string_to_int_area(self):
+        res = area(int("15"), int("4"))
+        self.assertEqual(res, 30)
+
+    def test_string_to_int_perimeter(self):
+        res = perimeter(int("5"), int("10"), int("6"))
+        self.assertEqual(res, 21)
+
+    def test_string_to_float_area(self):
+        res = area(float("5.5"), float("4.5"))
+        self.assertEqual(res, 12.375)
+
+    def test_string_to_float_perimeter(self):
+        res = perimeter(float("5.5"), float("4.5"), float("3.5"))
+        self.assertEqual(res, 13.5)
+
