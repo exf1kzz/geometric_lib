160fcdfba5a52bd5af75ed862610bab215d06e59
exf1kzz <141450846+exf1kzz@users.noreply.github.com>
2024-12-04 11:17:05 +0300

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 import unittest
 import circle
-import rectangle
+import reactangle
 import square
 import triangle
 
